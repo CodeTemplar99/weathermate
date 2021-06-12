5ec5166062b40720c1727a2cb12a4d961bf97e0e
Codetemplar99 <udoagwac@gmail.com>
2021-06-13 00:51:16 +0100

    nav bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CurrentWeatherComponent
+    CurrentWeatherComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
