ddf6017d4b85fa500af375c12c45862df1b1b848
Codetemplar99 <udoagwac@gmail.com>
2021-06-13 06:02:53 +0100

    weather functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,52 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import {
+//   MatButtonModule,
+//   MatToolbarModule,
+//   MatInputModule,
+//   MatTabsModule,
+//   MatCardModule,
+//   MatDividerModule,
+//   MatListModule
+// } from '@angular/material';
+import { HomePageComponent } from './home-page/home-page.component';
+import { StoreModule } from '@ngrx/store';
+import { locationReducer } from './reducers/reducer';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-
+import { FormsModule } from '@angular/forms';
+import { WeatherService } from './services/weather.service';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
+    HomePageComponent,
+    NavBarComponent,
     CurrentWeatherComponent,
-    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // AppRoutingModule,
+    BrowserAnimationsModule,
+    // MatButtonModule,
+    // MatToolbarModule,
+    StoreModule.forRoot({
+      loc: locationReducer
+    }),
+    FormsModule,
+    // MatInputModule,
+    // MatTabsModule,
+    // MatCardModule,
+    HttpClientModule,
+    // MatDividerModule,
+    // MatListModule
   ],
-  providers: [],
+  providers: [
+    WeatherService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Store } from '@ngrx/store';
+import { SET_LOCATION } from '../reducers/reducer';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  loc!: string;
+constructor(private store: Store<any>) { }
+ngOnInit() {
   }
-
+  search(searchForm: NgForm) {
+    if (searchForm.invalid) {
+      return;
+    }
+    this.store.dispatch({ type: SET_LOCATION, payload: this.loc });
+  }
 }
