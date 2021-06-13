e515b52b2910db3d17fe95996694d94ee10fb4ed
Codetemplar99 <udoagwac@gmail.com>
2021-06-13 20:29:16 +0100

    served on https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiKey: '2c5cf5457bb6fbda9722e545c4e1bcc5',
-  apiUrl: 'http://api.openweathermap.org/data/2.5'
+  apiUrl: 'https://api.openweathermap.org/data/2.5'
 };
 
 /*
