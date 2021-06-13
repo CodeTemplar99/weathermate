ca93b8f5234f8f95998b855f15524dc2d1e115dc
Codetemplar99 <udoagwac@gmail.com>
2021-06-13 18:31:13 +0100

    no forecast

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import * as moment from 'moment';
+// import * as moment from 'moment';
 const apiKey: string = environment.apiKey;
 @Injectable({
   providedIn: 'root'
@@ -10,13 +10,5 @@ export class WeatherService {
   constructor(private http: HttpClient) { }
   getCurrentWeather(loc: string) {
     return this.http.get(`${environment.apiUrl}/weather?q=${loc}&appid=${apiKey}`)
-  }
-  getForecast(loc: string) {
-    return this.http.get(`${environment.apiUrl}/forecast?q=${loc}&appid=${apiKey}`)
-  }
-  getUv(lat: number, lon: number) {
-    let startDate = Math.round(+moment(new Date()).subtract(1, 'week').toDate() / 1000);
-    let endDate = Math.round(+moment(new Date()).add(1, 'week').toDate() / 1000);
-    return this.http.get(`${environment.apiUrl}/uvi/history?lat=${lat}&lon=${lon}&start=${startDate}&end=${endDate}&appid=${apiKey}`)
   }
 }
